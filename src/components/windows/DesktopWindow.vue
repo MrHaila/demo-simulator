9f2bf886905880f4d62e635420a8dd3844939451
Teemu Haila <>
2023-01-14 22:18:27 +0200

    Scene refactor

---
+++ src/components/windows/DesktopWindow.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+div(class="px-2")
+  //- Logo
+  div(class="flex justify-center my-6")
+    EliteOsLogo(class="fill-gray-600 w-96")
+
+  //- Divider
+  div(class="flex my-6 items-center gap-4 text-gray-600")
+    p(class="uppercase") All Software
+    hr(class="flex-grow border-gray-600")
+  
+  //- Apps
+  div(class="flex justify-evenly space-x-2")
+    OsAppShortcut(
+      title="Scene Explorer"
+      description="The definitive source of all Scene challenges and compos."
+      buttonLabel="Open  Challenges"
+      @button="gameStateStore.currentEliteOsApp = EliteOsApps.ChallengesList"
+      )
+    OsAppShortcut(
+      title="Monkey Works X Pro"
+      description="You have been assigned a floating license for Monkey Works X Pro by Koko Group Global Ltd."
+      buttonLabel="Open Work"
+      @button="gameStateStore.currentEliteOsApp = EliteOsApps.Work"
+      )
+    OsAppShortcut(
+      title="Kokoshop"
+      description="Itching for skills? Old Koko has what you need... if you've got the scratch for it."
+      buttonLabel="Open Shop"
+      @button="gameStateStore.currentEliteOsApp = EliteOsApps.Desktop"
+      )
+    OsAppShortcut(
+      title="silk_road.app"
+      description="This is application is from an unidentified developer. It is not recommended to run this application."
+      buttonLabel="Open App"
+      @button="gameStateStore.currentEliteOsApp = EliteOsApps.Desktop"
+      )
+
+  OsNarrativeScene(
+    v-if="gameStateStore.talkingHeadQueues.desktop.length > 0"
+    :narrativeScene="gameStateStore.talkingHeadQueues.desktop[0]"
+    @done="gameStateStore.talkingHeadQueues.desktop.shift()"
+    )
+</template>
+
+<script lang="ts" setup>
+import OsAppShortcut from '@/components/OsAppShortcut.vue'
+import EliteOsLogo from '@/assets/EliteOsLogo.vue'
+import OsNarrativeScene from '@/components/OsNarrativeScene.vue'
+
+import { useGameStateStore } from '@/stores/gameStateStore'
+import { EliteOsApps } from '@/stores/gameStateStore'
+
+const gameStateStore = useGameStateStore()
+</script>
--- src/states/EliteOsScene.vue
+++ src/states/EliteOsScene.vue
@@ -2,62 +2,31 @@
 //- TODO
   - Confetti? https://confetti.js.org/#
 
+ParticlesComponent(
+  id="tsparticles"
+  class="absolute top-0 left-0 w-full h-full -z-50"
+  :particlesInit="particlesInit"
+  :options="particleOptions"
+  )
+
 div(class="p-2 flex gap-2 h-full")
   //- Left column
   ChallengesWindow(
     v-if="gameStateStore.currentEliteOsApp === EliteOsApps.ChallengesList"
     class="grow"
-  )
-  CodeEditorWindow(
+    )
+  ChallengeWindow(
     v-else-if="gameStateStore.currentEliteOsApp === EliteOsApps.Challenge"
     class="grow"
-  )
+    )
   MonkeyWorkWindow(
     v-else-if="gameStateStore.currentEliteOsApp === EliteOsApps.Work"
     class="grow"
-  )
-  div(
-    v-else
-    class="grow px-2"
-  )
-    ParticlesComponent(
-      id="tsparticles"
-      class="absolute top-0 left-0 w-full h-full -z-50"
-      :particlesInit="particlesInit"
-      :options="particleOptions"
     )
-
-    div(class="flex justify-center my-6")
-      EliteOsLogo(class="fill-gray-600 w-96")
-
-    div(class="flex my-6 items-center gap-4 text-gray-600")
-      p(class="uppercase") All Software
-      hr(class="flex-grow border-gray-600")
-    div(class="flex justify-evenly space-x-2")
-      OsAppShortcut(
-        title="Scene Explorer"
-        description="The definitive source of all Scene challenges and compos."
-        buttonLabel="Open  Challenges"
-        @button="gameStateStore.currentEliteOsApp = EliteOsApps.ChallengesList"
-        )
-      OsAppShortcut(
-        title="Monkey Works X Pro"
-        description="You have been assigned a floating license for Monkey Works X Pro by Koko Group Global Ltd."
-        buttonLabel="Open Work"
-        @button="gameStateStore.currentEliteOsApp = EliteOsApps.Work"
-        )
-      OsAppShortcut(
-        title="Kokoshop"
-        description="Itching for skills? Old Koko has what you need... if you've got the scratch for it."
-        buttonLabel="Open Shop"
-        @button="gameStateStore.currentEliteOsApp = EliteOsApps.Desktop"
-        )
-      OsAppShortcut(
-        title="silk_road.app"
-        description="This is application is from an unidentified developer. It is not recommended to run this application."
-        buttonLabel="Open App"
-        @button="gameStateStore.currentEliteOsApp = EliteOsApps.Desktop"
-        )
+  DesktopWindow(
+    v-else
+    class="grow"
+    )
 
   //- Right column
   div(
@@ -96,12 +65,6 @@ div(class="p-2 flex gap-2 h-full")
       ul
         li 1. Hello World
 
-OsNarrativeScene(
-  v-if="gameStateStore.talkingHeadQueues.desktop.length > 0"
-  :narrativeScene="gameStateStore.talkingHeadQueues.desktop[0]"
-  @done="gameStateStore.talkingHeadQueues.desktop.shift()"
-  )
-
 //- TODO: fade
 div(:class="['absolute bottom-0 left-0 right-0 top-0 bg-neutral-900 opacity-70 z-10 flex flex-col justify-end', { 'hidden': !isOverlayVisible }]")
   div(class="w-full text-right text-xs p-2 text-gray-400") DEBUG: {{ totalSubscribers }} subscriber{{ totalSubscribers === 1 ? '' : 's'}}
@@ -109,15 +72,13 @@ div(:class="['absolute bottom-0 left-0 right-0 top-0 bg-neutral-900 opacity-70 z
 </template>
 
 <script lang="ts" setup>
-import CodeEditorWindow from '@/components/windows/ChallengeWindow.vue'
+import ChallengeWindow from '@/components/windows/ChallengeWindow.vue'
 import MonkeyWorkWindow from '@/components/windows/MonkeyWorkWindow.vue'
 import ChallengesWindow from '@/components/windows/ChallengesListWindow.vue'
+import DesktopWindow from '@/components/windows/DesktopWindow.vue'
 import OsWindow from '@/components/windows/OsWindow.vue'
 import { useGameStateStore } from '@/stores/gameStateStore'
 import { EliteOsApps } from '@/stores/gameStateStore'
-import OsAppShortcut from '@/components/OsAppShortcut.vue'
-import EliteOsLogo from '@/assets/EliteOsLogo.vue'
-import OsNarrativeScene from '@/components/OsNarrativeScene.vue'
 
 import { useOverlay } from '@/components/composables/OsOverlay'
 
